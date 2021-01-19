e7060317e90c7e9f032b2411bdaf0d7c37e0d2b1
dengithub-dev <75982647+dengithub-dev@users.noreply.github.com>
2021-01-19 13:44:38 +0800

    Update login.php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ if(isset($_POST['login']))  //name of global form variable
     $run=mysqli_query($dbcon,$check_user);  //check the database rows for username and password input
     $rowsph = mysqli_num_rows($run);
     if ($rowsph == 1){ //validate a rows if it exist
-        echo "<script> window.open('welcome.php','_self')</script>"; //if there is a existing 1 row, open a welcome page
+        echo "<script> window.open('welcome.php','_self')</script>"; //if there is a match, it open a welcome page
         $_SESSION['username_input']=$username; 
     }  
     elseif ($rowsph >= 1){ //check for duplicates
