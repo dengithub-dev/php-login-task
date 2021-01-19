7f09dc52bca8f333e7393ca31cee473be583f586
dengithub-dev <75982647+dengithub-dev@users.noreply.github.com>
2021-01-19 11:48:41 +0800

    Update login.php

--- login.php
+++ login.php
@@ -1,16 +1,16 @@
 <?php  
 session_start();//session starts here  
 include("db_connection.php");  //call db_connection to connect to the database 
-if(isset($_POST['phpfilename']))  
+if(isset($_POST['login']))  //name your global form variable
 {  
-    $username0=$_POST['username_input'];  
-    $password0=$_POST['password_input'];  
-    $check_user="select * from table_login_form WHERE user='$username0' AND pass='$password0'";  //Run the query
+    $username=$_POST['username_input'];  
+    $password=$_POST['password_input'];  
+    $check_user="select * from table_login_form WHERE user='$username' AND pass='$password'";  //Run the query
     $run=mysqli_query($dbcon,$check_user);  //check the database rows for username and password input
     $rowsph = mysqli_num_rows($run);
     if ($rowsph == 1){ //validate a rows if it exist
         echo "<script> window.open('welcome.php','_self')</script>"; //if there is a existing 1 row, open a welcome page
-        $_SESSION['username_input']=$username0; 
+        $_SESSION['username_input']=$username; 
     }  
     elseif ($rowsph > 1){ //check for duplicates
         echo "<script>alert('Too many account with the same credential')</script>";
